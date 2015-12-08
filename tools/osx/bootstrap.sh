c5ea53fa5941cd6d0b69a7458c4dce2e5649b33b
zeroyonichihachi <tkfm.yamaguchi@gmail.com>
2015-12-07 21:29:18 -0800

    Added: bootstrapping script for OSX

---
+++ tools/osx/bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+
+rake bootstrap
+
+mkdir $HOME/local
+
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install golang
+
+export GOPATH=$HOME/local
+
+go get github.com/motemen/ghq
+go get github.com/peco/peco/cmd/peco
+go get github.com/svent/sift
+
+#ghq get git@github.com:zeroyonichihachi/dotfiles.git
+ghq get https://github.com/zeroyonichihachi/dotfiles.git
+rake clean
+
+cd ${GOPATH}/src/github.com/zeroyonichihachi/dotfiles
+rake bootstrap
