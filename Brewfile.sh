f4ccdcc49308c1400331bb88f74150a7edf82deb
zeroyonichihachi <tkfm.yamaguchi@gmail.com>
2019-03-27 10:12:49 +0900

    [BREW] Fixed: via migrating to mbp 2018

--- Brewfile.sh
+++ Brewfile.sh
@@ -18,101 +18,66 @@ brew cleanup
 ## add repositories
 
 brew tap sanemat/font
-brew tap peco/peco
-brew tap railwaycat/homebrew-emacsmacport
 brew tap caskroom/fonts
 
 
 
 ## brew softwares
 
-brew install curl
-# brew install gcc    # consuming resources a lot
-brew install git
-brew install go --cross-complie-common
-brew install imagemagick
-brew install rbenv ruby-build
-brew install tmux
-brew install reattach-to-user-namespace
-brew install wget
-brew install zsh
-brew install mysql
-brew install htop-osx
-brew install peco
-brew install nkf
-brew install pt
-brew install wine
-brew install winetricks
-brew install zenity
-brew install scala
-brew install ctags
-brew install nvm
-brew install pyenv
-brew install jq
-brew install opam
-brew install tree
-brew install thefuck
-brew install ocaml
-brew install oniguruma
-brew install openblas --build-from-source
-brew install R --with-openblas
-brew install ghostscript
-brew install pandoc
-brew install colordiff
-brew install git-archive-all
-brew install emacs-mac --with-spacemacs-icon
-brew install shellcheck
-brew install haskell-stack
-brew install jo
-brew install ccat
-brew install the_silver_searcher
-brew install the_plutinum_searcher
-brew install pigz
-brew install ffmpeg
-brew install macvim --with-lua --with-luajit
-brew install watch
-brew install ripgrep
-brew install gibo
+brew install \
+	R \
+	ccat \
+	colordiff \
+	ctags \
+       	curl \
+	ffmpeg \
+	ghostscript \
+	gibo \
+	git \
+	git-archive-all \
+	htop-osx \
+	imagemagick \
+	jo \
+	jq \
+	macvim \
+	nkf \
+	nvm \
+	oniguruma \
+	openblas \
+	pandoc \
+	peco \
+	pigz \
+	pt \
+	rbenv ruby-build \
+	reattach-to-user-namespace \
+	ripgrep \
+	shellcheck \
+	thefuck \
+	tmux \
+	tree \
+	watch \
+	wget \
+	zsh
+
+#brew install gcc    # consuming resources a lot
 
 
 ## cask softwares
 
 brew install caskroom/cask/brew-cask
 
-brew cask install alfred
-brew cask install atom
-brew cask install bettertouchtool
-brew cask install cyberduck
-brew cask install firefox
-brew cask install gimp
-brew cask install google-chrome chrome-remote-desktop-host
-brew cask install google-japanese-ime
-brew cask install inkscape
-brew cask install insomniax
-brew cask install iterm2
-brew cask install java
-brew cask install karabiner
-brew cask install libreoffice
-brew cask install owasp-zap
-brew cask install scroll-reverser
-brew cask install skype
-brew cask install sourcetree
-brew cask install vagrant
-brew cask install virtualbox
-brew cask install xquartz
-#brew cask install dropbox
-brew cask install spectacle
-brew cask install hyperswitch
-brew cask install font-cica
-brew cask install shiftit
-brew cask install reactotron
+brew cask install \
+	alfred
+	bettertouchtool \
+	firefox \
+	gimp \
+	google-chrome chrome-remote-desktop-host \
+	google-japanese-ime \
+	inkscape \
+	iterm2 \
+	karabiner-elements \
+	owasp-zap \
+	scroll-reverser \
+	font-cica \
+	xquartz
 
-
-
-# ricty installation depends on 'xquartz'
-brew install --vim-powerline ricty
-
-
-
-# wrap up
-brew linkapps
